d16a516ff6e8a1df91ea9713d0398714ebc7176e
runjin326 <RunJin@MacBook-Air.local>
2021-07-07 08:08:14 -0400

    files added from ../../../data folder

--- analyses/ge-only-based-clustering/expected_counts/run_clustering_CC_using_expectedcounts.sh
+++ analyses/ge-only-based-clustering/expected_counts/run_clustering_CC_using_expectedcounts.sh
@@ -1,51 +1,51 @@
 # spearman HC
 # olfd run - Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID" -l hgat_all_primary.tsv -d "spearman" -a "hc"  -o output/CC/Distance_spearman_finalLinkage_average_clusterAlg_HC
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "spearman" -a "hc" \
+-l input/initial_hgat_histology.tsv -d "spearman" -a "hc" \
 -o output/CC/Distance_spearman_finalLinkage_average_clusterAlg_HC_UQ-pgQ2 \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform UQ
 
 # spearman PAM
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "spearman" -a "pam" \
+-l input/initial_hgat_histology.tsv -d "spearman" -a "pam" \
 -o output/CC/Distance_spearman_finalLinkage_average_clusterAlg_PAM_UQ-pgQ2  \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform UQ
 
 # euclidean HC
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "euclidean" -a "hc" \
+-l input/initial_hgat_histology.tsv -d "euclidean" -a "hc" \
 -o output/CC/Distance_euclidean_finalLinkage_average_clusterAlg_HC_UQ-pgQ2  \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform UQ
 
 # euclidian PAM
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "euclidean" -a "pam" \
+-l input/initial_hgat_histology.tsv -d "euclidean" -a "pam" \
 -o output/CC/Distance_euclidean_finalLinkage_average_clusterAlg_PAM_UQ-pgQ2  \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform UQ
 
 # euclidean KM
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "euclidean" -a "km" \
+-l input/initial_hgat_histology.tsv -d "euclidean" -a "km" \
 -o output/CC/Distance_euclidean_finalLinkage_average_clusterAlg_KM_UQ-pgQ2  \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform UQ
 
 # manhattan PAM
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "manhattan" -a "pam" \
+-l input/initial_hgat_histology.tsv -d "manhattan" -a "pam" \
 -o output/CC/Distance_manhattan_finalLinkage_average_clusterAlg_PAM_UQ-pgQ2  \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform UQ
 
 # manhattan HC
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "manhattan" -a "hc" \
+-l input/initial_hgat_histology.tsv -d "manhattan" -a "hc" \
 -o output/CC/Distance_manhattan_finalLinkage_average_clusterAlg_HC_UQ-pgQ2 \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform UQ
 
 
@@ -53,51 +53,51 @@ Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospe
 # VST normalization
 # spearman HC
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "spearman" -a "hc" \
+-l input/initial_hgat_histology.tsv -d "spearman" -a "hc" \
 -o output/CC/Distance_spearman_finalLinkage_average_clusterAlg_HC_expct_counts_VST \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform vst
 
 # spearman PAM
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "spearman" -a "pam" \
+-l input/initial_hgat_histology.tsv -d "spearman" -a "pam" \
 -o output/CC/Distance_spearman_finalLinkage_average_clusterAlg_PAM_expct_counts_VST  \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform vst
 
 # euclidean HC
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "euclidean" -a "hc" \
+-l input/initial_hgat_histology.tsv -d "euclidean" -a "hc" \
 -o output/CC/Distance_euclidean_finalLinkage_average_clusterAlg_HC_expct_counts_VST  \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform vst
 
 # euclidian PAM
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "euclidean" -a "pam" \
+-l input/initial_hgat_histology.tsv -d "euclidean" -a "pam" \
 -o output/CC/Distance_euclidean_finalLinkage_average_clusterAlg_PAM_expct_counts_VST  \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform vst
 
 # euclidean KM
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "euclidean" -a "km" \
+-l input/initial_hgat_histology.tsv -d "euclidean" -a "km" \
 -o output/CC/Distance_euclidean_finalLinkage_average_clusterAlg_KM_expct_counts_VST  \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform vst
 
 # manhattan HC
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "manhattan" -a "hc" \
+-l input/initial_hgat_histology.tsv -d "manhattan" -a "hc" \
 -o output/CC/Distance_manhattan_finalLinkage_average_clusterAlg_HC_VST \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform vst
 
 # manhattan PAM
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "manhattan" -a "pam" \
+-l input/initial_hgat_histology.tsv -d "manhattan" -a "pam" \
 -o output/CC/Distance_manhattan_finalLinkage_average_clusterAlg_PAM_expct_counts_VST  \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform vst
 
 
@@ -106,51 +106,51 @@ Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospe
 ## Using edgeR  normalization
 # spearman HC
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "spearman" -a "hc" \
+-l input/initial_hgat_histology.tsv -d "spearman" -a "hc" \
 -o output/CC/Distance_spearman_finalLinkage_average_clusterAlg_HC_expct_counts_edgeR \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform edgeR
 
 # spearman PAM
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "spearman" -a "pam" \
+-l input/initial_hgat_histology.tsv -d "spearman" -a "pam" \
 -o output/CC/Distance_spearman_finalLinkage_average_clusterAlg_PAM_expct_counts_edgeR  \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform edgeR
 
 # euclidean HC
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "euclidean" -a "hc" \
+-l input/initial_hgat_histology.tsv -d "euclidean" -a "hc" \
 -o output/CC/Distance_euclidean_finalLinkage_average_clusterAlg_HC_expct_counts_edgeR  \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform edgeR
 
 # euclidian PAM
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "euclidean" -a "pam" \
+-l input/initial_hgat_histology.tsv -d "euclidean" -a "pam" \
 -o output/CC/Distance_euclidean_finalLinkage_average_clusterAlg_PAM_expct_counts_edgeR  \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform edgeR
 
 # euclidean KM
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "euclidean" -a "km" \
+-l input/initial_hgat_histology.tsv -d "euclidean" -a "km" \
 -o output/CC/Distance_euclidean_finalLinkage_average_clusterAlg_KM_expct_counts_edgeR  \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform edgeR
 
 # manhattan HC
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "manhattan" -a "hc" \
+-l input/initial_hgat_histology.tsv -d "manhattan" -a "hc" \
 -o output/CC/Distance_manhattan_finalLinkage_average_clusterAlg_HC_edgeR \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform edgeR
 
 # manhattan PAM
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "manhattan" -a "pam" \
+-l input/initial_hgat_histology.tsv -d "manhattan" -a "pam" \
 -o output/CC/Distance_manhattan_finalLinkage_average_clusterAlg_PAM_expct_counts_edgeR  \
---gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
+--gene_expression_file  input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform edgeR
 
 #
--- analyses/ge-only-based-clustering/expected_counts/run_clustering_heatmaps_and_stats_usingexpectedcounts.sh
+++ analyses/ge-only-based-clustering/expected_counts/run_clustering_heatmaps_and_stats_usingexpectedcounts.sh
@@ -1,28 +1,28 @@
 ## Subset expected counts RDS file
 Rscript code/01-subset-cohort-samples.R \
--i ../../../data/pbta-gene-counts-rsem-expected_count-collapsed.combined.rds  \
--p ../../../data/pbta-histologies.tsv \
--c ../../../data/initial_hgat_histology.tsv \
--o  ../../../data/initial_hgat-pbta-gene-counts-rsem-expected_count.rds \
--t ../../../data/initial_hgat-pbta-gene-counts-rsem-expected_count.tsv
+-i input/pbta-gene-counts-rsem-expected_count-collapsed.combined.rds  \
+-p input/pbta-histologies.tsv \
+-c input/initial_hgat_histology.tsv \
+-o input/initial_hgat-pbta-gene-counts-rsem-expected_count.rds \
+-t input/initial_hgat-pbta-gene-counts-rsem-expected_count.tsv
 
 ## Batch correction for samples of interest
 Rscript code/02-batch-correct.R \
---metadata ../../../data/initial_hgat_histology.tsv \
---mat ../../../data/initial_hgat-pbta-gene-counts-rsem-expected_count.rds \
+--metadata input/initial_hgat_histology.tsv \
+--mat input/initial_hgat-pbta-gene-counts-rsem-expected_count.rds \
 --output_prefix initial_hgat-pbta-gene-counts-rsem-expected_count 
 
 ## Filter genes in matrix to protein coding only
 Rscript code/03-filter-protein-coding.R \
---input ../../../data/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.rds \
+--input input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.rds \
 --gencode_version 27 \
---output ../../../data/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds
+--output input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds
 
 ## The following scripts will be run after the parameters are selected based on: 
 ## run_clustering_CC_using_expectedcounts.sh
 ## Clustering using CC using UQ.pgQ2
 Rscript code/04-clustering-CC.R --cohort_id "initial_hgat" -p "Kids_First_Biospecimen_ID"   \
--l initial_hgat_histology.tsv -d "spearman" -a "pam" \
+-l input/initial_hgat_histology.tsv -d "spearman" -a "pam" \
 -o output/CC/Distance_euclidean_finalLinkage_average_clusterAlg_KM_expct_counts_VST  \
 --gene_expression_file  initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds \
 --transform vst
@@ -32,13 +32,13 @@ Rscript code/05-heatmaps.R \
 -r output/CC/Distance_spearman_finalLinkage_average_clusterAlg_PAM_expct_counts_edgeR/initial_hgat/CC.Rdata \
 -p "Kids_First_Biospecimen_ID" -c input/color_withPCAcolor_3clusters.yaml \
 -o CC_based_heatmap_Distance_spearman_finalLinkage_average_clusterAlg_PAM_expct_counts_edgeR  \
--i ../../../data/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds  \
--f ../../../data/initial_hgat_histology.tsv -t YES --nclust 5
+-i input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.rds  \
+-f input/initial_hgat_histology.tsv -t YES --nclust 5
 
 
 # VTEST to pick immportant features
 python3 code/06-vtest.py \
--i ../../../data/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.tsv \
+-i input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding.tsv \
 -c output/CC_based_heatmap_Distance_spearman_finalLinkage_average_clusterAlg_PAM_expct_counts_edgeR_cluster_and_annotation.tsv  \
 -o stats/vtest/CC_based_heatmap_Distance_spearman_finalLinkage_average_clusterAlg_PAM_expct_counts_edgeR_VTEST \
 -v stats/vtest/CC_based_heatmap_Distance_spearman_finalLinkage_average_clusterAlg_PAM_expct_counts_edgeR_VTEST.tsv \
@@ -47,7 +47,7 @@ python3 code/06-vtest.py \
 
 ## CHoose top features per cluster
 python3 code/07-vtest-above-vscore2.py \
--i ../../../data/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding_top10k.tsv \
+-i input/initial_hgat-pbta-gene-counts-rsem-expected_count_corrected.proteincoding_top10k.tsv \
 -c  output/CC_based_heatmap_Distance_spearman_finalLinkage_average_clusterAlg_PAM_expct_counts_edgeR_cluster_and_annotation.tsv \
 -t CC -v output/overrep_analysis/CC_based_heatmap_Distance_spearman_finalLinkage_average_clusterAlg_PAM_expct_counts_edgeR_VTEST.tsv
 
